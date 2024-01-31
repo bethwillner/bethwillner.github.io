33f2ea9c31b9c0a048a897ea1672874c6e4486c2
bethwillner <156834203+bethwillner@users.noreply.github.com>
2024-01-31 12:01:54 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,23 @@
 console.log('hi')
+
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+if(isMorning == true)
+{
+    document.querySelector('#welcome h2').textContent = "Good Morning"
+}
+
+else if (isAfternoon == true)
+{
+    document.querySelector('#welcome h2').textContent = "Good Afternoon"
+}
+
+else if (isEvening == true)
+{
+    document.querySelector('#welcome h2').textContent = "Good Evening"
+}
